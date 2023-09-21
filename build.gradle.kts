371a4d6061d5e7aff4d19226d9ea7f2ebd29f55d
Amey Bansod <ameygotemail@gmail.com>
2023-09-20 20:37:35 -0400

    removed dotenv

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,8 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
     id("com.google.dagger.hilt.android") version "2.47" apply false
-    id("earth.levi.dotenv-android") version "1.3.0"
 }
-dotenv {
-    // the package name that is added to the top of your source code file: `import X.Y.Z`
-    packageName = "com.amey.weatherzip"
-    // the path to the source code in your Android app module. This is probably `src/main/java` but could be something else like `src/main/kotlin`
-    sourcePath = "src/main/java/"
-}
+
 buildscript {
 
     dependencies {
